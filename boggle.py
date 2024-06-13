2f8c71c94c290dc0ff544b04c401f372f8248e4f
byeoh100 <rax745@gmail.com>
2024-06-12 23:36:45 -0500

    Add grid search algo, incomplete

--- boggle.py
+++ boggle.py
@@ -47,17 +47,40 @@ class BoggleBoard:
 		for row_i, row in enumerate(self._board_list):
 			if word[0] in row:
 				col_i = row.index(word[0])
-				first_letters.append({row_i, col_i})
-				if not self.check_sides(word, row_i, col_i):
-					continue
+				first_letters.append([row_i, col_i])
 
-		print(first_letters)
+		result = self.word_checker(word, first_letters)
+		return result
 
-	def word_checker(self):
-		pass
+	def word_checker(self, word, first_letters):
+		next_letter_i = 0
+
+		for i in first_letters:
+			row_i = [i[0]]
+			col_i = [i[1]]
+			path = []
+			path.append([row_i, col_i])
+			next_letter_i += 1
+			used_i = []
+
+			while len(path) > 0:
+				if next_letter_i == len(word):
+					return path
+				last_ele = path[-1] # error here
+				path_head_row = last_ele[0]
+				path_head_col = last_ele[1]
+				search = self.check_sides(word[next_letter_i], path_head_row, path_head_col, used_i)
+				if search == -1:
+					used_i.append([path_head_row, path_head_col])
+					path.pop()
+					next_letter_i -= 1
+				else:
+					path.append(search)
+					next_letter_i += 1
+			
+			return -1
 	
-	def check_sides(self, word, row_i, col_i):
-		next_letter_i = []
+	def check_sides(self, next_letter, row_i, col_i, used_i):
 		for row in range(-1, 2):
 			for col in range(-1, 2):
 				new_row_i = row_i + row
@@ -65,11 +88,12 @@ class BoggleBoard:
 				x_bound = len(self._board_list) - 1
 				y_bound = len(self._board_list[row_i]) - 1
 				
-				if new_row_i > x_bound or new_row_i < 0 or new_col_i > y_bound or new_col_i < 0 or row == 0 and col == 0:
+				if new_row_i > x_bound or new_row_i < 0 or new_col_i > y_bound or new_col_i < 0 or row == 0 and col == 0 or [new_row_i, new_col_i] in used_i:
 					continue
-				if self._board_list[new_row_i][new_col_i] == word[1]:
-					next_letter_i.append({new_row_i:new_col_i})
-		return next_letter_i
+				if self._board_list[new_row_i][new_col_i] == next_letter:
+					return [new_row_i, new_col_i]
+		
+		return -1
 
 def print_board():
 	for i in board1.board:
@@ -79,5 +103,5 @@ board1 = BoggleBoard()
 board1.shake()
 print_board()
 word = input("Enter word: ")
-board1.first_letter_finder(word.upper())
+print(board1.first_letter_finder(word.upper()))
 pass
